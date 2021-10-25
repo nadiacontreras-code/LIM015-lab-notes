4b5e3d14e16735288a08575c4ab1dae0447ec971
nadiacontreras-code <nadiabrenda.cs@gmail.com>
2021-10-25 10:29:20 -0500

    creando el componente contador en vue

---
+++ lab-notes/src/components/contador.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{counter}}</h1>
+   <!--  si quiero enlazarlo con la data (counter) del script, pues
+    lo llamo con las dobles llaves "{{}}" -->
+    <button @click="suma">+</button>
+    <!-- para llamar a los eventos se recurre al "@" -->
+    <button @click="resta">-</button>
+  </div>
+</template>
+
+<script>
+// import {  } from ''
+
+export default {
+  data() {
+    return {
+      counter:0
+    }
+  },
+  methods:{
+    //funciones para la data
+    suma(){ //1era funcion como objeto
+      this.counter++
+      /*"this" te trae cualquier elemento que este dentro dentro de
+      data o de methods */
+    },
+    resta(){
+      this.counter--
+    }
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
